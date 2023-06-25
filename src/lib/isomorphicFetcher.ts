78a90b629bb8fb36b28c445010cffc78dd6cc0a2
gabrielelpidio <gabrieldeandradeleal@gmail.com>
2023-06-25 17:49:09 -0400

    fix: issues with isomorphic fetch

--- src/lib/isomorphicFetcher.ts
+++ src/lib/isomorphicFetcher.ts
@@ -1,15 +1,13 @@
 import { type TypedDocumentNode } from "@graphql-typed-document-node/core";
 import { GraphQLClient } from "graphql-request";
 
-const API_URL = "https://mock.shop/api";
-
-const { request } = new GraphQLClient(API_URL);
+const GQLClient = new GraphQLClient(process.env.NEXT_PUBLIC_API_URL!);
 
 const client = <TResult, TVariables>(
   document: TypedDocumentNode<TResult, TVariables>,
   variables?: TVariables
 ): Promise<TResult> => {
-  return request(document, variables ?? undefined);
+  return GQLClient.request(document, variables ?? undefined);
 };
 
 export default client;
